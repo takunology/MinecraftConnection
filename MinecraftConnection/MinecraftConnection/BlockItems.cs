993acfb5d8428181dc478f772d8b18df49b19af4
たくのろじぃ / Takunology <takunology.net@gmail.com>
2020-12-27 11:35:57 +0900

    クラスの仕分け
    - アイテムの分類を行うためのクラスを作成

--- MinecraftConnection/MinecraftConnection/BlockItems.cs
+++ MinecraftConnection/MinecraftConnection/BlockItems.cs
@@ -39,6 +39,46 @@ namespace MinecraftConnection
         /// 磨かれた安山岩
         /// </summary>
         public static readonly string PolishedAndesite = "minecraft:polished_andesite";
+        /// <summary>
+        /// 草ブロック
+        /// </summary>
+        public static readonly string GrassBlock = "minecraft:grass_block";
+        /// <summary>
+        /// 土ブロック
+        /// </summary>
+        public static readonly string Dirt = "minecraft:dirt_block";
+        /// <summary>
+        /// 粗い土ブロック
+        /// </summary>
+        public static readonly string CoarseDIrt = "minecraft:coarse_dirt";
+        /// <summary>
+        /// ポドゾル
+        /// </summary>
+        public static readonly string Podzol = "minecraft:podzol";
+        /// <summary>
+        /// 丸石
+        /// </summary>
+        public static readonly string CobbleStone = "minecraft:cobblestone";
+        /// <summary>
+        /// オークの木材
+        /// </summary>
+        public static readonly string OakPlanks = "minecraft:oak_planks";
+        /// <summary>
+        /// マツの木材
+        /// </summary>
+        public static readonly string SprucePlanks = "minecraft:spruce_planks";
+        /// <summary>
+        /// シラカバの木材
+        /// </summary>
+        public static readonly string BirchPlanks = "minecraft:birch_planks";
+        /// <summary>
+        /// ジャングルの木材
+        /// </summary>
+        public static readonly string JunglePlanks = "minecraft:jungle_planks";
+        /// <summary>
+        /// アカシアの木材
+        /// </summary>
+        public static readonly string AcaciaPlanks = "minecraft:acacia_planks";
 
     }
 }
---
+++ MinecraftConnection/MinecraftConnection/Stones.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinecraftConnection
+{
+    public class Stones : IBlockItems
+    {
+
+    }
+}
