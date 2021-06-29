2beaa7aa51f8dc1980f0b4bcaf9502be20ad5cb1
たくのろじぃ / Takunology <takunology.net@gmail.com>
2021-06-30 03:13:04 +0900

    Release Build 1.0.0-beta1

--- MinecraftConnection/ExampleApp/Program.cs
+++ MinecraftConnection/ExampleApp/Program.cs
@@ -12,35 +12,34 @@ namespace ExampleApp
 {
     class Program
     {
-        private static IPAddress Address = IPAddress.Parse("127.0.0.1");
-        private static ushort Port = 25575;
-        private static string Pass = "minecraft";
-        private static MinecraftCommands command = new MinecraftCommands(Address, Port, Pass);
+        private static readonly IPAddress _address = IPAddress.Parse("127.0.0.1");
+        private static readonly ushort _port = 25575;
+        private static readonly string _pass = "minecraft";
+        private static MinecraftCommands command = new MinecraftCommands(_address, _port, _pass);
 
         static void Main(string[] args)
         {
-            string PlayerName = "takunology";
-            
-            var PlayerData = command.GetPlayerData(PlayerName);
-            int x = PlayerData.PositionX + 30;
-            int y = PlayerData.PositionY - 10;
-            int z = PlayerData.PositionZ;
+            string playerName = "takunology";
 
-            List<FireworksColors> color_1 = new List<FireworksColors>() { FireworksColors.CYAN };
-            List<FireworksColors> color_2 = new List<FireworksColors>() { FireworksColors.BLUE };
-            Fireworks fireworks = new Fireworks(0, 2, FireworksShapes.Burst, false, true, color_1, color_2);
+            var playerData = command.GetPlayerData(playerName);
+            int x = playerData.PositionX + 30;
+            int y = playerData.PositionY - 10;
+            int z = playerData.PositionZ;
 
-            while (true)
+            List<FireworksColors> excolor = new List<FireworksColors>() { FireworksColors.BLUE };
+            List<FireworksColors> fadecolor = new List<FireworksColors>() { FireworksColors.CYAN };
+
+            Fireworks fireworks = new Fireworks(0, 2, FireworksShapes.Burst, false, true, excolor, fadecolor);
+
+            for(int i = 0; i < 10; i++)
             {
-                for (int i = 0; i < 10; i++)
-                {
-                    command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, 1.0, 3.0));
-                    command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, -1.0, -3.0));
-                    command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, -1.0, 3.0));
-                    command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, 1.0, -3.0));
-                    command.Wait(200);
-                }
+                command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, 1.0, 3.0));
+                command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, -1.0, -3.0));
+                command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, -1.0, 3.0));
+                command.SetOffFireworks(x, y + i * 2, z, fireworks.Motion(0.0, 1.0, -3.0));
+                command.Wait(200);
             }
+            
         }
     }
 }
